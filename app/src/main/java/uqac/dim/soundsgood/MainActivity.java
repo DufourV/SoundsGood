1cf9c5eae72fc537bd0577d179e86a2042750ceb
JeanFelix5 <jf_girard5@hotmail.ca>
2022-03-31 15:36:18 -0400

    Ajout de l'activité de la liste de musique

--- app/src/main/java/uqac/dim/soundsgood/MainActivity.java
+++ app/src/main/java/uqac/dim/soundsgood/MainActivity.java
@@ -2,6 +2,7 @@ package uqac.dim.soundsgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -12,8 +13,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK){
+                        Intent intent = result.getData();
+
+                        if(intent != null) {
+                            //extract data
+
+                        }
+                    }
+                }
+            }
+    );
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void openActivityListeMusique() {
+        Intent intent = new Intent(this, ListeMusique.class);
+        activityLauncher.launch(intent);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -34,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         {
             case R.id.menu_ListeMusique:
                 Log.i("DIM", "VOICI VOTRE LISTE DE MUSIQUE!");
-
+                openActivityListeMusique();
 
                 return true;
 
