e6f920323026cc8147a051608cba284ebc086d4f
Oustaycache <felbrassard@gmail.com>
2022-04-01 16:27:52 -0400

    create table for parameters

--- app/src/main/java/uqac/dim/soundsgood/Parametres.java
+++ app/src/main/java/uqac/dim/soundsgood/Parametres.java
@@ -2,7 +2,13 @@ package uqac.dim.soundsgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class Parametres extends AppCompatActivity {
 
